37c93563f576a629fafe652a54a9d13a22465417
walkercole <besidethebrave@gmail.com>
2024-11-12 14:31:26 -0800

    update unfurling

--- components/SeoHead.js
+++ components/SeoHead.js
@@ -14,8 +14,8 @@ const defaultMeta = {
   robots: 'follow, index',
   // change with url of your image (recommended dimension = 1.91:1)
   // used in twitter, facebook, etc. card when link copied in tweet/status 
-  image: 'https://next-landing-vpn.vercel.app/assets/card-image.png',
-  author: 'Lorem Ipsum'
+  image: '../public/assets/termnestUnfurl.png',
+  author: 'TermNest'
 };
 
 /**
